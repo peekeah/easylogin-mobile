785bbe9a44e357b48b619ef1ca93331a3022ce6f
Pranay Kothari <pranay.kothari109@gmail.com>
2023-12-16 14:02:54 +0530

    style: update tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      blue: '#0A0171',
-      green: '#2BC990'
+    extend: {
+      colors: {
+        blue: '#0A0171',
+        green: '#2BC990'
+      },
     },
-    extend: {},
   },
   plugins: []
-  
+
 }
