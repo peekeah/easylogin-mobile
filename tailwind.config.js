4e1d4380db5b4de2f27602d7b7279e45a06a8f60
Pranay Kothari <pranay.kothari109@gmail.com>
2023-12-16 13:02:07 +0530

    feat: add custom colors in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      blue: '#0A0171',
+      green: '#2BC990'
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: []
+  
 }
